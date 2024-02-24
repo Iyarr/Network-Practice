5231ada04e61d37a1e20885a5b11379ed3eb575b
Iyarr <8.24ridiculous@gmail.com>
2024-02-24 17:30:51 +0900

    ファイルの簡略化

--- basic/docker.sh
+++ basic/docker.sh
@@ -1,18 +1,24 @@
 #!sh
 
-docker-compose down --rmi all --volumes
+rm() {
+    docker-compose down --rmi all --volumes
+}
 
 no_cache_up() {
+    rm
     docker-compose build --no-cache
     docker-compose up -d
 }
 
 cache_up() {
+    rm
     docker-compose build
     docker-compose up -d
 }
 
-if [ "$1" = "up" ]; then
+if [ "$1" = "rm" ]; then
+    rm
+elif [ "$1" = "up" ]; then
     cache_up
 elif [ "$1" = "no-cache" ]; then
     no_cache_up
